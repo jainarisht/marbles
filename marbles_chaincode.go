a459a96d831b9d2f59a656a317063912080ebe4d
Arisht Jain <arisht.jain@xooa.com>
2018-08-13 21:11:53 +0530

    c

--- marbles_chaincode.go
+++ marbles_chaincode.go
@@ -179,14 +179,14 @@ func (t *SimpleAsset) queryLocation(stub shim.ChaincodeStubInterface, args []str
 // It retrieves all the history of the device for particular date.
 func (t *SimpleAsset) queryByDate(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 
-	if len(args) < 3 {
+	if len(args) < 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 3")
 	}
 
 	locationId := args[0]
 	deviceId := args[1]
-	date := args[2]
-	queryString := fmt.Sprintf("{\r\n    \"selector\": {\r\n        \"docType\": \"Event\",\r\n        \"locationId\": \"%s\",\r\n        \"deviceId\": \"%s\",\r\n        \"date\": \"%s\"\r\n    },\r\n    \"fields\": [\"value\",\"time\"]\r\n}", locationId, deviceId, date)
+	// date := args[2]
+	queryString := fmt.Sprintf("{\r\n    \"selector\": {\r\n        \"docType\": \"Event\",\r\n        \"locationId\": \"%s\",\r\n        \"deviceId\": \"%s\"\r\n    },\r\n    \"fields\": [\"value\",\"time\"]\r\n}", locationId, deviceId)
 	// queryString := fmt.Sprintf("{\"selector\":{\"docType\":\"Event\",\"locationId\":\"%s\"},\r\n    \"fields\": [\"value\",\"time\"]\r\n,\r\n    \"sort\": [{\"time\":\"desc\"}]\r\n}", locationId)
 
 	queryResults, err := getQueryResultForQueryString(stub, queryString)
